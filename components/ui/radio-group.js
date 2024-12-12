ec9766d62083e9e4a55bd1cee86d6ba4a1d3d983
ryanheenan <101992238+ryanheenan@users.noreply.github.com>
2024-12-12 09:23:57 -0800

    Update radio-group.js

--- components/ui/radio-group.js
+++ components/ui/radio-group.js
@@ -1,7 +1,7 @@
-export const RadioGroup = ({ children }) => (
-  <div className="flex space-x-4">{children}</div>
+export const RadioGroup = ({ children, className }) => (
+  <div className={`flex space-x-4 ${className}`}>{children}</div>
 );
 
 export const RadioGroupItem = ({ value, id }) => (
-  <input type="radio" value={value} id={id} name="radio-group" />
+  <input type="radio" id={id} value={value} className="form-radio" />
 );
