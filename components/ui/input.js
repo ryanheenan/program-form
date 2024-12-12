3af8c2f7495e6104b43d60ea1e2cf611e7da02c1
ryanheenan <101992238+ryanheenan@users.noreply.github.com>
2024-12-12 09:23:33 -0800

    Update input.js

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,3 +1,6 @@
 export const Input = (props) => (
-  <input {...props} className="border p-2 rounded w-full" />
+  <input
+    {...props}
+    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+  />
 );
