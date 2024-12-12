f9da1d79b2c18425305cb618eea4e956adc72fb9
ryanheenan <101992238+ryanheenan@users.noreply.github.com>
2024-12-12 09:23:22 -0800

    Update label.js

--- components/ui/label.js
+++ components/ui/label.js
@@ -1,5 +1,8 @@
 export const Label = ({ children, htmlFor }) => (
-  <label htmlFor={htmlFor} className="block text-sm font-bold">
+  <label
+    htmlFor={htmlFor}
+    className="block text-sm font-medium text-gray-700"
+  >
     {children}
   </label>
 );
