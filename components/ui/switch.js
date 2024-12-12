80c158c25b7d9d360c77bbf80b8097cbf49e5149
ryanheenan <101992238+ryanheenan@users.noreply.github.com>
2024-12-12 09:23:42 -0800

    Update switch.js

--- components/ui/switch.js
+++ components/ui/switch.js
@@ -3,5 +3,6 @@ export const Switch = ({ checked, onCheckedChange }) => (
     type="checkbox"
     checked={checked}
     onChange={(e) => onCheckedChange(e.target.checked)}
+    className="form-checkbox h-5 w-5 text-indigo-600"
   />
 );
